ee5601c8716d6f18ad09d282426d6255078df265
graypeck <Graydon.Peck@Ally.com>
2018-06-19 14:59:22 -0400

    'Completed' controlles for actors and movies

--- src/main/java/com/el/ally/wimp/api/ActorAPIController.java
+++ src/main/java/com/el/ally/wimp/api/ActorAPIController.java
@@ -1,8 +1,18 @@
 package com.el.ally.wimp.api;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.el.ally.wimp.models.Actor;
+import com.el.ally.wimp.models.Movie;
 import com.el.ally.wimp.repositories.ActorRepository;
 import com.el.ally.wimp.repositories.MovieRepository;
 
@@ -16,6 +26,34 @@ public class ActorAPIController {
 	public ActorAPIController(ActorRepository actorRepository, MovieRepository movieRepository) {
 		this.actorRepository = actorRepository;
 		this.movieRepository = movieRepository;
+	}
+	
+	@GetMapping("")
+	public List<Actor> getAll(){
+		return actorRepository.findAll();
+	}
+	
+	@GetMapping("{id}")
+	public Actor getOne(@PathVariable int id){
+		return actorRepository.findOne(id);
+	}
+	
+	@PostMapping("")
+	public Actor create(@RequestBody Actor actor) {
+		return actorRepository.save(actor);
+	}
+	
+	@PutMapping("{id}")
+	public Actor update(@RequestBody Actor actor,@PathVariable int id) {
+		actor.setId(id);
+		return actorRepository.save(actor);
+	}
+	
+	@DeleteMapping("{id}")
+	public Actor delete(@PathVariable int id) {
+		Actor actor = actorRepository.findOne(id);
+		actorRepository.delete(id);
+		return actor;
 	}
 
 
--- src/main/java/com/el/ally/wimp/api/MovieAPIController.java
+++ src/main/java/com/el/ally/wimp/api/MovieAPIController.java
@@ -1,8 +1,17 @@
 package com.el.ally.wimp.api;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.el.ally.wimp.models.Movie;
 import com.el.ally.wimp.repositories.ActorRepository;
 import com.el.ally.wimp.repositories.MovieRepository;
 
@@ -16,5 +25,33 @@ public class MovieAPIController {
 	public MovieAPIController(MovieRepository movieRepository, ActorRepository actorRepository) {
 		this.movieRepository = movieRepository;
 		this.actorRepository = actorRepository;
+	}
+	
+	@GetMapping("")
+	public List<Movie> getAll(){
+		return movieRepository.findAll();
+	}
+	
+	@GetMapping("{id}")
+	public Movie getOne(@PathVariable int id){
+		return movieRepository.findOne(id);
+	}
+	
+	@PostMapping("")
+	public Movie create(@RequestBody Movie movie) {
+		return movieRepository.save(movie);
+	}
+	
+	@PutMapping("{id}")
+	public Movie update(@RequestBody Movie movie,@PathVariable int id) {
+		movie.setId(id);
+		return movieRepository.save(movie);
+	}
+	
+	@DeleteMapping("{id}")
+	public Movie delete(@PathVariable int id) {
+		Movie movie = movieRepository.findOne(id);
+		movieRepository.delete(id);
+		return movie;
 	}
 }
