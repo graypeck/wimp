97795bce00959c440e94881290352b1384bb4f9c
graypeck <Graydon.Peck@Ally.com>
2018-06-19 14:25:17 -0400

    Commit number 2, for the Wan and Imran

--- src/main/java/com/el/ally/wimp/api/ActorController.java
+++ src/main/java/com/el/ally/wimp/api/ActorController.java
@@ -1,8 +1,17 @@
 package com.el.ally.wimp.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ActorController {
+import com.el.ally.wimp.repositories.ActorRepository;
+import com.el.ally.wimp.repositories.MovieRepository;
 
+@RestController
+@RequestMapping
+public class ActorController {
+	
+	private ActorRepository actorRepository;
+	private MovieRepository movieRepository;
+	
+	
 }
--- src/main/java/com/el/ally/wimp/models/Actor.java
+++ src/main/java/com/el/ally/wimp/models/Actor.java
@@ -6,13 +6,14 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="actor")
 public class Actor {
-
+	@Id
 	@GeneratedValue(generator="actor_id_seq", strategy=GenerationType.AUTO)
 	@SequenceGenerator(name="actor_id_seq", sequenceName="actor_id_seq")
 	private int id;
